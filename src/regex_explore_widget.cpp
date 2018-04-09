08d5ec1295354d08064a6d3dd1afb994140c1046
Anthony Leedom <anthonym.leedom@gmail.com>
2018-04-08 23:43:09 -0500

    Add submatch data from regex in Regex_explore_widget

--- src/regex_explore_widget.cpp
+++ src/regex_explore_widget.cpp
@@ -7,6 +7,7 @@
 #include <cppurses/painter/glyph_string.hpp>
 #include <signals/slot.hpp>
 
+#include <regex_explore/match.hpp>
 #include <regex_explore/textbox_highlight.hpp>
 
 using cppurses::Glyph_string;
@@ -49,16 +50,25 @@ void Regex_explore_widget::perform_search_and_update() {
 
     // Clear the current tb_highlight of ranges.
     target_text_section_.tb_highlight.clear_all_highlights();
+    bottom_bar_.submatch_display.clear_all_matches();
 
     // Search target text with above regex in a loop
     for (std::sregex_iterator i{std::sregex_iterator(
              std::begin(target_text), std::end(target_text), regex)};
          i != std::sregex_iterator(); ++i) {
         std::smatch match{*i};
-        Range r{static_cast<std::size_t>(match.position(0)),
-                static_cast<std::size_t>(match.length(0))};
-        target_text_section_.tb_highlight.add_highlight(r);
-        // add sub_matches to some data struct.
+        // Entire Match
+        Range range_entire{static_cast<std::size_t>(match.position(0)),
+                           static_cast<std::size_t>(match.length(0))};
+        target_text_section_.tb_highlight.add_highlight(range_entire);
+
+        // Submatches
+        Match match_data{range_entire};
+        for (std::size_t i{1}; i < match.size(); ++i) {
+            Range sub_range{static_cast<std::size_t>(match.position(i)),
+                            static_cast<std::size_t>(match.length(i))};
+            match_data.submatches.push_back(sub_range);
+        }
     }
 }
 
